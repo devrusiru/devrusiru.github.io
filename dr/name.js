d39d3170dca65c679c43cba0e10b4451749b4256
devrusiru <136108931+devrusiru@users.noreply.github.com>
2024-06-05 02:51:52 +0530

    Add files via upload

---
+++ dr/name.js
@@ -0,0 +1,6 @@
+ var typed = new Typed(".auto-input-name",{
+                        strings: ["CV", "RESUME", "CURRICULUM VITAE"],
+                        typeSpeed: 100,
+                        backSpeed: 100,
+                        loop: true,
+                    });
