28669fe6cabf0d8352af44b98c2c9234c7d981e6
devrusiru <136108931+devrusiru@users.noreply.github.com>
2024-06-05 02:12:56 +0530

    Delete name.js

--- name.js
+++
@@ -1,6 +0,0 @@
- var typed = new Typed(".auto-input-name",{
-                        strings: ["CV", "RESUME", "CURRICULUM VITAE"],
-                        typeSpeed: 100,
-                        backSpeed: 100,
-                        loop: true,
-                    });
