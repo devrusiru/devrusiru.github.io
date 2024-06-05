2281837865ae46d7c248ee6914232de83c364469
devrusiru <136108931+devrusiru@users.noreply.github.com>
2024-06-05 21:19:49 +0530

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,7 +1,9 @@
 $(".body-wrapper").hide();
+$(".nav-title-wrappe").hide();
 $(window).on("load", function () {
   // makes sure the whole site is loaded
 
   $("#preloader").delay(5000).fadeOut("slow");
   $(".body-wrapper").delay(5000).fadeIn("slow");
+  $(".nav-title-wrappe").delay(5000).fadeIn("slow");
 });
