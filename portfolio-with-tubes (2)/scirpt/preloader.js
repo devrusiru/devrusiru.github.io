ed601a56bcc58df7ac64a99df39814d7056e7685
devrusiru <136108931+devrusiru@users.noreply.github.com>
2026-02-18 17:58:52 +0530

    Delete portfolio-with-tubes (2) directory

--- portfolio-with-tubes (2)/scirpt/preloader.js
+++
@@ -1,9 +0,0 @@
-//$(".body-wrapper").hide();
-$(".nav-title-wrappe").hide();
-$(window).on("load", function () {
-  // makes sure the whole site is loaded
-
-  $("#preloader").delay(5000).fadeOut("slow");
-  $(".body-wrapper").delay(5000).fadeIn("slow");
-  $(".nav-title-wrappe").delay(5000).fadeIn("slow");
-});
